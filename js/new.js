4eadf696d134b5eece9ccd4d7c0ded3184154a4a
Николай Лабазов <Nickolaylabazov@yandex.ru>
2020-02-28 23:23:30 +0300

    q

--- js/new.js
+++ js/new.js
@@ -18,10 +18,15 @@ $(document).ready(function(){
 	
 	$('.tabs-nav-new').children('li').filter(':first').addClass('active');
 	$('.tabs-box-new').children('div').filter(':first').addClass('active');
-	
+
+	$('.tabs-nav-new1').children('li').filter(':first').addClass('active');
+	$('.tabs-box-new1').children('div').filter(':first').addClass('active');
+
+	/* 28.02.20 */
+
 	$('.tabs-nav-new li').click(function(e) {
 		var a = $(this),
-				parent = a.parents('.tabs-new'),
+				parent = a.parents('.tabs-new1'),
 				nav = parent.children('.tabs-nav-new').children('li'),
 				box = parent.children('.tabs-box-new').children('div');
 
@@ -34,7 +39,27 @@ $(document).ready(function(){
 		}
 
 		e.preventDefault();
-	});
+	}); 
+
+	$('.tabs-nav-new1 li').click(function(e) {
+		var a = $(this),
+				parent = a.parents('.tabs-new1'),
+				nav = parent.children('.tabs-nav-new1').children('li'),
+				box = parent.children('.tabs-box-new1').children('div');
+
+		if (!a.hasClass('active')) {
+			a.addClass('active')
+				.siblings().removeClass('active');
+
+			box.eq(a.index()).addClass('active')
+				.siblings().removeClass('active');
+		}
+
+		e.preventDefault();
+	}); 
+
+	/* 28.02.20 */
+	
 	
 	$('.new-car_list_item_content').hide();
   $('.new-car_list_item_title').on('click', function() {
